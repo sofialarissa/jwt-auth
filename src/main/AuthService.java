addb6b43b13b1045c2346c19e666a91809838ef4
LuisVittor <164118126+LuisVittor@users.noreply.github.com>
2026-03-24 18:56:17 -0300

    Update welcome message in AuthService

--- src/main/AuthService.java
+++ src/main/AuthService.java
@@ -1,5 +1,5 @@
 public class AuthService {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!\nSeja bem-vindo ao Jwt-auth!");
+        System.out.println("Olá, Mundo!!!\nSeja bem-vindo ao Jwt-auth!!!");
     }
 }
